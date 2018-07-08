7ac971081c1a53a02dfe84cfd857fbe445127d46
Mr.X <512573042@qq.com>
2018-07-09 00:08:13 +0800

    添加留言回复接口

--- components/messageItem/index.js
+++ components/messageItem/index.js
@@ -3,11 +3,42 @@ Component({
         listData: Array
     },
     methods: {
-        longpress: function(e) {
-            wx.showModal({
-                title: 'aa',
-                content: 'bb',
-            })
+        longpress: function (e) {
+            console.log("a");
+            if (e.currentTarget.dataset.del)
+                wx.showModal({
+                    title: '确定删除吗',
+                    cancelText: '取消',
+                    confirmText: '确定',
+                    confirmColor: "#007aff",
+                    success: (e1) => {
+                        if (e1.confirm) {
+                            wx.request({
+                                url: "https://www.dszejt.com/ws/ws_xcx.asmx/Set_XCX_YSZX_LYDEL",
+                                data: {
+                                    strLYID: e.currentTarget.dataset.id,
+                                    strLYRID: e.currentTarget.dataset.sfzh,
+                                    strKey: "C1BC7666E5C74BD384196-AD1532102C1"
+                                },
+                                method: "POST",
+                                dataType: "JSON",
+                                success: (res) => {
+                                    var result = JSON.parse(JSON.parse(res.data).d);
+                                    if (result.code == 0) {
+                                        wx.showToast({
+                                            title: '成功删除',
+                                        });
+                                        var index = e.currentTarget.dataset.index;
+                                        this.data.listData.splice(index, 1);
+                                        this.setData({
+                                            listData: this.data.listData
+                                        })
+                                    }
+                                }
+                            })
+                        }
+                    }
+                })
         }
     }
 })
--- pages/consult/talking/index.js
+++ pages/consult/talking/index.js
@@ -2,13 +2,16 @@ const app = getApp();
 
 Page({
     data: {
+        content: null,
+        zx: null,
         user: null,
         doctor: null,
         messages: null
     },
     onLoad: function (options) {
         this.setData({
-            doctor: app.globalData.userinfo
+            doctor: app.globalData.userinfo,
+            zx: options['ID']
         })
         wx.request({
             url: "https://www.dszejt.com/ws/ws_xcx.asmx/Get_XCX_YSZX_User_ZXInfo",
@@ -36,6 +39,44 @@ Page({
                 this.setData({
                     messages: JSON.parse(JSON.parse(res.data).d)
                 })
+            }
+        })
+    },
+    change: function (e) {
+        this.setData({
+            content: e.detail.value
+        })
+    },
+    submit: function () {
+        wx.request({
+            url: "https://www.dszejt.com/ws/ws_xcx.asmx/Set_XCX_YSZX_LYSC",
+            data: {
+                strZXID: this.data.zx,
+                strLYDX: "Y",
+                strLYRID: this.data.doctor['医生ID'],
+                strNR: this.data.content,
+                strKey: "C1BC7666E5C74BD384196-AD1532102C1"
+            },
+            method: "POST",
+            dataType: "JSON",
+            success: (res) => {
+                var result = JSON.parse(JSON.parse(res.data).d);
+                if (result.code == 0) {
+                    this.data.messages.push({
+                        '留言ID': result['留言ID'],
+                        '内容': this.data.content,
+                        '留言对象': 'Y',
+                        '留言人ID': this.data.doctor['医生ID'],
+                        '姓名': this.data.doctor['姓名'],
+                        '头像': this.data.doctor['头像']
+                    })
+                    this.setData({
+                        messages: this.data.messages,
+                        content: ''
+                    })
+                } else {
+                    // 留言失败
+                }
             }
         })
     }
